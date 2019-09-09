0c940a52585a53af0f3186aaaaa465ec59fa38cb
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-09-10 01:01:16 +1000

    Sound Management, Text Generation fixed
    Added some basic sound management including the ability to enable/disable sound, and choose a custom sound.  Tidied up some of the codebase and procedurised the text formatting.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmMain
+Partial Class FrmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,47 +23,94 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
-        Me.niClipClear = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.cmsClipClear = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.NiClipClear = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.CmsClipClear = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.ClearClipboardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.cmsClipClear.SuspendLayout()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SoundToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectCustomSoundToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.OFSound = New System.Windows.Forms.OpenFileDialog()
+        Me.tsmiUseDefault = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CmsClipClear.SuspendLayout()
         Me.SuspendLayout()
         '
-        'niClipClear
+        'NiClipClear
         '
-        Me.niClipClear.ContextMenuStrip = Me.cmsClipClear
-        Me.niClipClear.Icon = CType(resources.GetObject("niClipClear.Icon"), System.Drawing.Icon)
-        Me.niClipClear.Text = "ClipClear v0.1"
-        Me.niClipClear.Visible = True
+        Me.NiClipClear.ContextMenuStrip = Me.CmsClipClear
+        Me.NiClipClear.Icon = CType(resources.GetObject("NiClipClear.Icon"), System.Drawing.Icon)
+        Me.NiClipClear.Text = "ClipClear v0.1"
+        Me.NiClipClear.Visible = True
         '
-        'cmsClipClear
+        'CmsClipClear
         '
-        Me.cmsClipClear.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.cmsClipClear.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearClipboardToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
-        Me.cmsClipClear.Name = "cmsClipClear"
-        Me.cmsClipClear.Size = New System.Drawing.Size(207, 74)
+        Me.CmsClipClear.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.CmsClipClear.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearClipboardToolStripMenuItem, Me.ToolStripMenuItem1, Me.ToolStripMenuItem2, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.CmsClipClear.Name = "cmsClipClear"
+        Me.CmsClipClear.Size = New System.Drawing.Size(241, 145)
         '
         'ClearClipboardToolStripMenuItem
         '
         Me.ClearClipboardToolStripMenuItem.Name = "ClearClipboardToolStripMenuItem"
-        Me.ClearClipboardToolStripMenuItem.Size = New System.Drawing.Size(206, 32)
+        Me.ClearClipboardToolStripMenuItem.Size = New System.Drawing.Size(240, 32)
         Me.ClearClipboardToolStripMenuItem.Text = "Clear Clipboard"
         '
         'ToolStripMenuItem1
         '
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(203, 6)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(237, 6)
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(206, 32)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(240, 32)
         Me.ExitToolStripMenuItem.Text = "E&xit"
         '
-        'frmMain
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SoundToolStripMenuItem, Me.tsmiUseDefault, Me.ToolStripSeparator2, Me.SelectCustomSoundToolStripMenuItem})
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(240, 32)
+        Me.ToolStripMenuItem2.Text = "Options"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(237, 6)
+        '
+        'SoundToolStripMenuItem
+        '
+        Me.SoundToolStripMenuItem.Name = "SoundToolStripMenuItem"
+        Me.SoundToolStripMenuItem.Size = New System.Drawing.Size(296, 34)
+        Me.SoundToolStripMenuItem.Text = "Sound Enabled"
+        '
+        'SelectCustomSoundToolStripMenuItem
+        '
+        Me.SelectCustomSoundToolStripMenuItem.Name = "SelectCustomSoundToolStripMenuItem"
+        Me.SelectCustomSoundToolStripMenuItem.Size = New System.Drawing.Size(296, 34)
+        Me.SelectCustomSoundToolStripMenuItem.Text = "Select Custom Sound..."
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(293, 6)
+        '
+        'OFSound
+        '
+        Me.OFSound.AddExtension = False
+        Me.OFSound.Title = "Select Sound File"
+        '
+        'tsmiUseDefault
+        '
+        Me.tsmiUseDefault.Name = "tsmiUseDefault"
+        Me.tsmiUseDefault.Size = New System.Drawing.Size(296, 34)
+        Me.tsmiUseDefault.Text = "Use Default Sound"
+        '
+        'FrmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -71,18 +118,25 @@ Partial Class frmMain
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Name = "frmMain"
+        Me.Name = "FrmMain"
         Me.ShowInTaskbar = False
         Me.Text = "ClipClear"
         Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
-        Me.cmsClipClear.ResumeLayout(False)
+        Me.CmsClipClear.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents niClipClear As NotifyIcon
-    Friend WithEvents cmsClipClear As ContextMenuStrip
+    Friend WithEvents NiClipClear As NotifyIcon
+    Friend WithEvents CmsClipClear As ContextMenuStrip
     Friend WithEvents ClearClipboardToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
     Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents SoundToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectCustomSoundToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents OFSound As OpenFileDialog
+    Friend WithEvents tsmiUseDefault As ToolStripMenuItem
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,34 +1,92 @@
 ﻿Imports System.IO
 
-Public Class frmMain
+Public Class FrmMain
+    Public appVersion As String
+    Public RecycleSoundPath As String
+    Public DefaultSound As String
+
     Private Sub ClearClipboardToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearClipboardToolStripMenuItem.Click
-        doClipClear()
+        'Chose the Clear Clipboard menu item, lear the Clipboard
+        DoClipClear()
     End Sub
 
-    Private Sub niClipClear_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles niClipClear.MouseDoubleClick
-        doClipClear()
+    Private Sub niClipClear_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NiClipClear.MouseDoubleClick
+        'Double click on the NotifyIcon... Clear the Clipboard
+        DoClipClear()
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Visible = False
+
+        'Get the current build version and put it in a clean format
+        With My.Application.Info.Version
+            appVersion = String.Format(" v{0}.{1}.{2}", .Major, .Minor, .Build)
+        End With
+
+        'Set up the text captions
+        Me.Text = My.Settings.AppFriendlyName + appVersion
+        NiClipClear.Text = My.Settings.AppFriendlyName + appVersion
+        SoundToolStripMenuItem.Checked = My.Settings.ClearSoundEnabled
+
+        'Prep the Open Dialog Box
+        OFSound.Filter = "Wave files (*.wav)|*.wav|MP3 files (*.mp3)|*.mp3"
+        OFSound.InitialDirectory = Path.GetPathRoot(Environment.SystemDirectory) & "Windows\Media\"
+
+        'Init Sound Info
+        DefaultSound = Path.GetPathRoot(Environment.SystemDirectory) & "Windows\Media\recycle.wav"
+        If My.Settings.AltSoundPath = "" Then
+            RecycleSoundPath = DefaultSound
+            tsmiUseDefault.Enabled = False
+        Else
+            RecycleSoundPath = My.Settings.AltSoundPath
+            tsmiUseDefault.Enabled = True
+        End If
+
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        niClipClear.Dispose()
-        Application.Exit()
+        'We're done, lets exit cleanly.
+        NiClipClear.Dispose()
+        Close()
     End Sub
 
     Sub PlaySwishSound()
-        Dim RecycleSoundPath As String = Path.GetPathRoot(Environment.SystemDirectory) & "Windows\Media\recycle.wav"
+        'Play a background sound to advise that clipboard has been emptied
         My.Computer.Audio.Play(RecycleSoundPath, AudioPlayMode.Background)
     End Sub
 
-    Sub doClipClear()
+    Sub DoClipClear()
         Try
+            'Try the clipboard clear. If it works, play the sound (if its enabled)
             Clipboard.Clear()
-            PlaySwishSound()
+            If My.Settings.ClearSoundEnabled Then PlaySwishSound()
         Catch
-            MsgBox("Error clearing clipboard.", vbOKOnly & vbCritical, "ClipClear v0.1")
+            'This is unlikely to ever be called, but just in case... 
+            MsgBox("Error clearing clipboard.", vbOKOnly & vbCritical, My.Settings.AppFriendlyName + appVersion)
         End Try
+    End Sub
+
+    Private Sub SoundToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SoundToolStripMenuItem.Click
+        My.Settings.ClearSoundEnabled = Not (My.Settings.ClearSoundEnabled)
+        My.Settings.Save()
+        SoundToolStripMenuItem.Checked = My.Settings.ClearSoundEnabled
+    End Sub
+
+    Private Sub SelectCustomSoundToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectCustomSoundToolStripMenuItem.Click
+        If OFSound.ShowDialog <> DialogResult.Cancel Then
+            'Cancel not pressed, lets set up the new custom sound.
+            My.Settings.AltSoundPath = OFSound.FileName
+            My.Settings.Save()
+            RecycleSoundPath = OFSound.FileName
+            tsmiUseDefault.Enabled = True
+        End If
+    End Sub
+
+    Private Sub TsmiUseDefault_Click(sender As Object, e As EventArgs) Handles tsmiUseDefault.Click
+        'Revert back to the Default Sound
+        tsmiUseDefault.Enabled = False
+        My.Settings.AltSoundPath = ""
+        RecycleSoundPath = DefaultSound
+        My.Settings.Save()
     End Sub
 End Class
