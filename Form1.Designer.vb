d7b36229f7a3498d14f14baed5b46b71d73840e4
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-09-09 16:33:08 +1000

    Initial Commit
    Fixed duplicate folder issue.

---
+++ Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.niClipClear = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.cmsClipClear = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ClearClipboardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmsClipClear.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'niClipClear
+        '
+        Me.niClipClear.ContextMenuStrip = Me.cmsClipClear
+        Me.niClipClear.Icon = CType(resources.GetObject("niClipClear.Icon"), System.Drawing.Icon)
+        Me.niClipClear.Text = "ClipClear v0.1"
+        Me.niClipClear.Visible = True
+        '
+        'cmsClipClear
+        '
+        Me.cmsClipClear.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearClipboardToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.cmsClipClear.Name = "cmsClipClear"
+        Me.cmsClipClear.Size = New System.Drawing.Size(181, 76)
+        '
+        'ClearClipboardToolStripMenuItem
+        '
+        Me.ClearClipboardToolStripMenuItem.Name = "ClearClipboardToolStripMenuItem"
+        Me.ClearClipboardToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearClipboardToolStripMenuItem.Text = "Clear Clipboard"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 163)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.ShowInTaskbar = False
+        Me.Text = "ClipClear"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.cmsClipClear.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents niClipClear As NotifyIcon
+    Friend WithEvents cmsClipClear As ContextMenuStrip
+    Friend WithEvents ClearClipboardToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.IO
+
+Public Class frmMain
+    Private Sub ClearClipboardToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearClipboardToolStripMenuItem.Click
+        doClipClear()
+    End Sub
+
+    Private Sub niClipClear_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles niClipClear.MouseDoubleClick
+        doClipClear()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Visible = False
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        niClipClear.Dispose()
+        Application.Exit()
+    End Sub
+
+    Sub PlaySwishSound()
+        Dim RecycleSoundPath As String = Path.GetPathRoot(Environment.SystemDirectory) & "Windows\Media\recycle.wav"
+        My.Computer.Audio.Play(RecycleSoundPath, AudioPlayMode.Background)
+    End Sub
+
+    Sub doClipClear()
+        Try
+            Clipboard.Clear()
+            PlaySwishSound()
+        Catch
+            MsgBox("Error clearing clipboard.", vbOKOnly & vbCritical, "ClipClear v0.1")
+        End Try
+    End Sub
+End Class
